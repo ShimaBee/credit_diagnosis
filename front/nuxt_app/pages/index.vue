43e72bdac17cd8ce6fbd9967edb69cdd81e7519e
shimabee <mrs1122ani@gmail.com>
2020-02-24 17:08:42 +0900

    index.vueの大まかなレイアウト

--- front/nuxt_app/pages/index.vue
+++ front/nuxt_app/pages/index.vue
@@ -1,29 +1,29 @@
 <template>
-  <section class="d-flex justify-center mt-10">
-      <v-container>
-        <div class="top-content">
-          <p class="body-1 mb-7 mt-7 text">
-            自分に合った泡盛がわからない。
-            <br />と思っている人もたくさんいると思います。
-            <br />だって数百種類もあるとそうなって当然です。
-            <br />ただ、必ずあなたに合う泡盛は必ずあります。
-            <br />どの泡盛も魅力的だから。
-            <br />あなたが好む泡盛をご提案します。
-          </p>
-          <v-btn
-            width="230"
-            height="50"
-            rounded
-            class="cyan accent-2 mb-8 ma-4 mt-10 title"
-            to="/question"
-            nuxt
-          >診断スタート！</v-btn>
-        </div>
-      </v-container>
-    </section>
+  <v-container class="d-flex justify-center mt-10">
+    <div class="top-content">
+      <h1 class="d-flex justify-center ma-12">LOGO</h1>
+      <p class="body-1 mb-7 pt-7 text">
+        自分に最適なクレジットカードを見つけたい。
+        <br />と思っている人もたくさんいると思います。
+        <br />自分に最適なクレジットカードをてにいれて
+        <br />もっと得したい
+        <br />もっと決済を効率化したい
+        <br />そんなあなたに最適なクレジットカードをご提案します。
+      </p>
+      <div class="btn-wrapper d-flex justify-center">
+      <v-btn
+        width="230"
+        height="50"
+        rounded
+        class="cyan accent-2 mb-8 ma-4 mt-10 title"
+        to="/question"
+        nuxt
+      >診断スタート！</v-btn>
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
