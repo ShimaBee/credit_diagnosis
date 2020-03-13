10602e72f4e818076ba67339caad75474334b3ed
shimabee <mrs1122ani@gmail.com>
2020-03-13 11:40:27 +0900

    コードの説明のためのコメントをいれました

--- back/rails_api/app/controllers/answers_controller.rb
+++ back/rails_api/app/controllers/answers_controller.rb
@@ -1,12 +1,13 @@
 class AnswersController < ApplicationController
 
   def answers
-    # ユーザが選んだ選択肢を受け取る
+    # ユーザが選んだ選択肢を受け取る。
     choices = params[:choices]
-
-    # ユーザから受け取った選択肢の持つ値をyamlから取得する
+    # ユーザから受け取った選択肢の持つ値をyamlから取得する。
     questions = YAML.load_file("public/questions.yml")
+    # ユーザの選んだ選択肢(index)が持つ値を入れる配列
     answer_results = []
+    # ユーザが選んだ選択肢の持つ値をyamlから取って、配列に入れる。
     questions.each_with_index do |question, index|
       answer_results << question["values"][params[:choices][index]]
     end
