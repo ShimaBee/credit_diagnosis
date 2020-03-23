3d197403491289ddc32aa0a34b564a9bf48d18e3
shimabee <mrs1122ani@gmail.com>
2020-03-23 14:42:23 +0900

    上位三つのカードの順番をマッチ順にしました

--- back/rails_api/app/controllers/answers_controller.rb
+++ back/rails_api/app/controllers/answers_controller.rb
@@ -3,6 +3,8 @@ class AnswersController < ApplicationController
   def answers
     # ユーザが選んだ選択肢を受け取る。
     choices = params[:choices]
+
+
     # yamlの質問と選択肢を全て持ってくる。
     questions = YAML.load_file("public/questions.yml")
     # ユーザの選んだ選択肢(index)が持つ趣向値を入れる配列を作る。
@@ -11,35 +13,30 @@ class AnswersController < ApplicationController
     questions.each_with_index do |question, index|
       taste_values << question["values"][choices[index]]
     end
-
     # 趣向値をだけを配列に入れる
     user_taste_values = taste_values.map{|v| v.values}.flatten!
 
+
     # 全てのカードの趣向値を配列にいれる。
-    cards_compared_values_array = Card.pluck(:age, :points, :miles, :annual_fee)
-    # |ユーザ趣向値 - カードの趣向値| この計算を全カード分行う。
-    different_values = []
-    cards_compared_values_array.each do |c|
-      different_values << [user_taste_values, c].transpose.map{|a| a.inject(:-).abs }
+    cards_taste_values = Card.pluck(:age, :points, :miles, :annual_fee)
+
+
+    # |ユーザ趣向値 - カードの趣向値| この計算を全カード分行う。  
+    different_values = cards_taste_values.map do |v|
+      [user_taste_values, v].transpose.map{|a| a.inject(:-).abs}
     end
     # アンマッチ度の算出(arrayの中の足し算)
-    arrays_sum = []
-    different_values.each do |diff|
-      arrays_sum.push << diff.sum.round(2)
-    end
-
+    arrays_sum = different_values.map{|v| v.sum.round(2)}
     # アンマッチ度にindexをつけて、hash化する。
     sums_with_id = {}
     arrays_sum.each_with_index do |sum,index|
       sums_with_id.merge!(index+1 => sum)
     end
-    # アンマッチ度をもとにマッチしている順番に並べる。
-    much_index = sums_with_id.sort_by { |_, v| v }.to_h
-    # マッチしている上位三つのindexを取得する。
-    much_cards_index = much_index.keys.take(3)
-    
+    # アンマッチ度をもとにマッチしている順番に並べ、上位三つのindexを取得する。
+    much_cards_index = sums_with_id.sort_by { |_, v| v }.to_h.keys.take(3)
+
     # マッチしている上位三つのindexをもとに、DBからCardを引っ張る。
-    cards = Card.where(id: much_cards_index)
+    cards = Card.where(id: much_cards_index).order_as_specified(id: much_cards_index)
     # Nuxt側にjson形式でマッチしたcardの情報を返す。
     render json: cards, status: 200
   end
